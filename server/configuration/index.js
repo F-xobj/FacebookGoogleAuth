5a525b678a1b8386b2cb742adfc742a3aa51d396
Fares Fouad <fares.fouad@equityzen.com>
2020-07-15 12:00:49 +0300

    add congif file

---
+++ server/configuration/index.js
@@ -0,0 +1,15 @@
+module.exports = {
+  JWT_SECRET: "FXOBJ",
+  oauth: {
+    google: {
+      clientID:
+        "696567563959-7p8kts89voj9k01504jprs9o3aacfgqj.apps.googleusercontent.com",
+      clientSecret: "_UNhS-wIprJ1O-LfcGT9mMxp",
+    },
+    facebook: {
+      clientID: "2771619929761813",
+      clientSecret: "aa5671305137d182af3c99abffd94a76",
+    },
+  },
+  MONGOOSE_URI: "mongodb+srv://fares:9LOpWXTu8zRSpTQW@oauth.ndhw5.mongodb.net/Mern?retryWrites=true&w=majority"
+};

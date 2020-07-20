c92cfdfddd4ccd00ae1d00618da6e60a5e8fbd1e
F-xobj <fares.allefdawi78@gmail.com>
2020-07-20 15:55:25 +0300

    feat (hooks):dashboard

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,20 +1,12 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import * as actions from '../../Redux/actions/index'
-
-class Dashboard extends Component {
-  componentDidMount() {
-    this.props.getSecret()
-  }
-  render() {
-    return <>{this.props.secret}</>
-  }
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getSecret } from '../../Redux/actions/index'
+const Dashboard = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(getSecret())
+  }, [])
+  const secret = useSelector((state) => state.dashboard.secret)
+  return <>{secret}</>
 }
-
-const mapStateToProps = (state) => {
-  return {
-    secret: state.dashboard.secret,
-  }
-}
-
-export default connect(mapStateToProps, actions)(Dashboard)
+export default Dashboard
